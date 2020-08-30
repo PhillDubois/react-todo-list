a5168ee1a8b056567b5e73e4609c78854ff2f3f7
Louis Philippe Dubois <louis.philippe.dubois@outlook.com>
2020-08-30 23:30:22 +0200

    Css hotfixes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from "prop-types";
 import { Header } from "./components/layout/Header";
 import { Content } from "./components/layout/Content";
-import "./App.scss";
 import { ProjectsProvider, SelectedProjectProvider } from "./context";
+import "./App.scss";
 
-export const App = () => (
-  <SelectedProjectProvider>
-    <ProjectsProvider>
-      <div className="App">
-        <Header />
-        <Content />
-      </div>
-    </ProjectsProvider>
-  </SelectedProjectProvider>
-);
+export const App = ({ darkModeDefault = false }) => {
+  const [darkMode, setDarkMode] = useState(darkModeDefault);
+
+  return (
+    <SelectedProjectProvider>
+      <ProjectsProvider>
+        <main
+          data-testid="application"
+          className={darkMode ? "darkmode" : undefined}
+        >
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Content />
+        </main>
+      </ProjectsProvider>
+    </SelectedProjectProvider>
+  );
+};
+
+App.propTypes = {
+  darkModeDefault: PropTypes.bool,
+};
